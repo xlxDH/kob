9d39183f173c637f099caf18c40bbb886e4f6405
DH_xlx <v847588615@hotmail.com>
2023-10-31 14:48:05 +0800

    backened

---
+++ backened/src/main/java/com/kob/backened/BackenedApplication.java
@@ -0,0 +1,13 @@
+package com.kob.backened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackenedApplication.class, args);
+    }
+
+}
